c86cdcf241a45719c16406d77a13e23afea5d34e
Hamza Bham <92208535+MHamzaBham@users.noreply.github.com>
2025-01-14 20:40:21 +0500

    Handled action error on deleteInvoice

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { deleteInvoice } from '@/app/lib/actions';
@@ -26,11 +28,14 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
-  const DeleteInvoiceWithId = deleteInvoice.bind(null, id)
+  const handleDelete = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    await deleteInvoice(id);
+  };
   return (
     <>
-      <form action={DeleteInvoiceWithId}>
-        <button className="rounded-md border p-2 hover:bg-gray-100">
+      <form onSubmit={handleDelete}>
+        <button type='submit' className="rounded-md border p-2 hover:bg-gray-100">
           <span className="sr-only">Delete</span>
           <TrashIcon className="w-5" />
         </button>
