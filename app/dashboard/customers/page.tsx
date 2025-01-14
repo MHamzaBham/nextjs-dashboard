75e5d8e24997c9edfcad4804e1d037d834bb2202
Hamza Bham <92208535+MHamzaBham@users.noreply.github.com>
2025-01-14 20:13:41 +0500

    removed unused elements

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,7 +1,6 @@
-import { fetchCustomers, fetchFilteredCustomers, fetchInvoicesPages } from "@/app/lib/data"
+import { fetchInvoicesPages } from "@/app/lib/data"
 import CustomersTable from "@/app/ui/customers/table"
 import Pagination from "@/app/ui/invoices/pagination";
-import { CreateInvoice } from "@/app/ui/invoices/buttons";
 
 export default async function Page(props: {
   searchParams?: Promise<{
